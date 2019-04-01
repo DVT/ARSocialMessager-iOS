afd620f6950f63a31b20cfa707af129ba62b0f88
Marie Kristein-Harmsen <mharmsen.dvt@gmail.com>
2019-04-01 15:05:01 +0200

    Set up Firebase database Added gitignore file for userdata, userstate and pods

---
+++ Drop MemoryTests/Drop_MemoryTests.swift
@@ -0,0 +1,34 @@
+//
+//  Drop_MemoryTests.swift
+//  Drop MemoryTests
+//
+//  Created by Marie Kristein-Harmsen on 2019/04/01.
+//  Copyright © 2019 DVT. All rights reserved.
+//
+
+import XCTest
+@testable import Drop_Memory
+
+class Drop_MemoryTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
