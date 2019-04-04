adabff862044187eb57c60b516429ca15f0fa244
David Minders <dminders@jhb.dvt.co.za>
2019-04-04 16:32:22 +0200

    * Fixed load error * Added button and removed tap gesture * Displaying user input text

---
+++ Drop-Memory/Drop-Memory/TextHelper.swift
@@ -0,0 +1,13 @@
+//
+//  TextHelper.swift
+//  ArTest1
+//
+//  Created by Zaheer Moola on 2019/04/01.
+//  Copyright © 2019 DVT. All rights reserved.
+//
+
+import Foundation
+
+class TextHelper {
+    static var message: String = ""
+}
